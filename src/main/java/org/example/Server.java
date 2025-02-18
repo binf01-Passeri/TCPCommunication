a6d5f046ca94ee1322a918e729c342d55753426c
binf01-Passeri <105107000+binf01-Passeri@users.noreply.github.com>
2025-02-18 13:06:14 +0100

    Create Server.java

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progetto_tcp;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Riccardo
+ */
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private int porta;
+
+    public Server(int porta) {
+        this.porta = porta;
+    }
+
+    public Socket attendi() {
+
+    }
+
+    public void scrivi(String messaggio) {
+
+    }
+
+    public void leggi() {
+
+    }
+
+    public void chiudi() {
+
+    }
+
+    public void termina() {
+
+    }
+}
